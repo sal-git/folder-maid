a14dcf91febb618616c04368778b479626412d8d
sal-git <stephenlightcap121212@gmail.com>
2022-07-23 10:59:14 -0500

    structure rules somewhat

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,51 @@ use notify::{Watcher, RecursiveMode, watcher};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
-fn main() {
-    // Create a channel to receive the events.
-    let (tx, rx) = channel();
+struct Rule {
+    name: String,
+    condition: String,
+    category: String,
+    value: String
+}
 
-    // Create a watcher object, delivering debounced events.
-    // The notification back-end is selected based on the platform.
+struct Action {
+    name: String,
+    date_created: String,
+    rule: Rule,
+    category: String,
+    to: String,
+    directory: String
+}
+
+fn build_some_test_rules() -> Action {
+    let rule = Rule{
+        name: "Test".to_string(),
+        condition: "GTE".to_string(),
+        category: "SIZE".to_string(), 
+        value: "1KB".to_string()
+    };
+
+    let action = Action{
+        name: "Copy large files".to_string(),
+        date_created: "now".to_string(),
+        rule: rule,
+        category: "COPY".to_string(),
+        to: "/home/sal/Documents".to_string(),
+        directory: "/home/sal/Development/folder_maid/folder_maid".to_string() 
+    };
+
+    return action;
+}
+
+fn main() {
+    let (tx, rx) = channel();
+    
+    let action = build_some_test_rules();
+    
+    println!("{}", action.name);
+
     let mut watcher = watcher(tx, Duration::from_secs(10)).unwrap();
 
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
     watcher.watch("/home/sal/Development", RecursiveMode::Recursive).unwrap();
 
     loop {
@@ -23,64 +58,3 @@ fn main() {
         }
     }
 }
-/*
-use notify::{Watcher, RecursiveMode, Result};
-//use std::time::Duration;
-use std::path::Path;
-
-fn main() -> Result<()> {
-    //println!("Hello, world!");
-    let mut watcher = notify::recommended_watcher(|res| {
-        match res {
-            Ok(event) => println!("event: {:?}", event),
-            Err(e) => println!("watch error: {:?}", e),
-        }
-    })?;
-
-    watcher.watch(Path::new("/home/sal/Development/folder_maid/folder_maid"), RecursiveMode::Recursive)?;
-
-    Ok(())
-}
-*/
-
-// use watchman_client::prelude::*;
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let client = Connector::new().connect().await?;
-//     let resolved = client
-//         .resolve_root(CanonicalPath::canonicalize("/home/")?)
-//         .await?;
-
-//     let files = client.glob(&resolved, &["**/*.rs"]).await?;
-//     println!("files: {:#?}", files);
-//     Ok(())
-// }
-
-/*
-extern crate notify;
-use notify::{Watcher, RecursiveMode, RawEvent, raw_watcher};
-use std::sync::mpsc::channel;
-
-fn main() {
-    // Create a channel to receive the events.
-    let (tx, rx) = channel();
-
-    // Create a watcher object, delivering raw events.
-    // The notification back-end is selected based on the platform.
-    let mut watcher = raw_watcher(tx).unwrap();
-
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
-    watcher.watch("/home/sal/Development/folder_maid", RecursiveMode::Recursive).unwrap();
-
-    loop {
-        match rx.recv() {
-           Ok(RawEvent{path: Some(path), op: Ok(op), cookie}) => {
-               println!("{:?} {:?} ({:?})", op, path, cookie)
-           },
-           Ok(event) => println!("broken event: {:?}", event),
-           Err(e) => println!("watch error: {:?}", e),
-        }
-    }
-}
-*/
